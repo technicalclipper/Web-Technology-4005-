09892a99ab935e3928dcf8fe5161bba8db59e232
technical clipper <150263476+technicalclipper@users.noreply.github.com>
2024-11-25 22:43:49 +0530

    Add files via upload

---
+++ ex 5a/project/WEB-INF/classes/GamingServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GamingServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Gaming Setup</title></head>");
+        out.println("<body>");
+        out.println("<h1>Welcome to Gaming Setup Details</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
